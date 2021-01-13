09f4b61f347a8db493a703056c668c9a6ac931c3
VickyAH <vad5@leicester.ac.uk>
2021-01-13 17:48:12 +0000

    Bug Fix for when ChemicalShiftList contains no shifts to search against

--- modules/BackboneAssignmentModule.py
+++ modules/BackboneAssignmentModule.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -13,8 +13,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-11-06 19:17:39 +0000 (Fri, November 06, 2020) $"
+__modifiedBy__ = "$modifiedBy: VickyAH $"
+__dateModified__ = "$dateModified: 2021-01-13 17:48:13 +0000 (Wed, January 13, 2021) $"
 __version__ = "$Revision: 3.0.1 $"
 #=========================================================================================
 # Created
@@ -771,7 +771,8 @@ class BackboneAssignmentModule(NmrResidueTableModule):
         if chemicalShiftList:
             for nmrResidue in self.application.project.nmrResidues:
                 nmrAtoms = [nmrAtom for nmrAtom in nmrResidue.nmrAtoms]
-                shifts = [chemicalShiftList.getChemicalShift(atom.id) for atom in nmrAtoms]
+                shifts = [chemicalShiftList.getChemicalShift(atom.id) for atom in nmrAtoms
+                          if chemicalShiftList.getChemicalShift(atom.id) is not None]
                 if nmrResidue.relativeOffset == -1:
                     self.interShifts[nmrResidue] = shifts
                 elif nmrResidue.relativeOffset == 0 or nmrResidue.relativeOffset is None:
