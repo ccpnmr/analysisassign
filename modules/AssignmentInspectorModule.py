2a38b84d2dd48a4b5582a802d148c41d72aa8f0f
Ed Brooksbank <ejb66@le.ac.uk>
2021-01-14 18:56:44 +0000

    minor bug fix

--- modules/AssignmentInspectorModule.py
+++ modules/AssignmentInspectorModule.py
@@ -7,7 +7,7 @@ modified by Geerten 1-9/12/2016:
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -17,8 +17,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-10-07 17:12:46 +0100 (Wed, October 07, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2021-01-14 18:56:44 +0000 (Thu, January 14, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -657,6 +657,9 @@ class AssignmentInspectorTable(GuiTable):
         # self._registerNotifiers() - removed for testing
 
         self._peakList = None
+        self._nmrResidues = []
+        self._nmrAtoms = []
+
         # update if current.nmrResidue is defined
         if self.application.current.nmrResidue is not None and self.application.current.chemicalShiftList is not None:
             self._updateModuleCallback({NMRRESIDUES: [self.application.current.nmrResidue]},
