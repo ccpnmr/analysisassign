6097d6c798dc5ab79c8fefc58559aece749a3a18
Ed Brooksbank <ejb66@le.ac.uk>
2021-01-18 14:31:28 +0000

    added missing test for no displays selected

--- modules/PickAndAssignModule.py
+++ modules/PickAndAssignModule.py
@@ -12,7 +12,7 @@ and to include "Restricted pick and assign" button.
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -22,8 +22,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-09-22 09:33:22 +0100 (Tue, September 22, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2021-01-18 14:31:29 +0000 (Mon, January 18, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -216,10 +216,13 @@ class PickAndAssignModule(NmrResidueTableModule):
             nmrResidue = self.application.current.nmrResidue
 
         if nmrResidue is None:
-            logger.error('Undefined nmrResidue; select one first before proceeding')
+            logger.warning('Undefined nmrResidue; select one first before proceeding')
             return
 
-        currentAxisCodes = self.nmrResidueTableSettings.axisCodeOptions.getSelectedText()
+        if not self.nmrResidueTableSettings.axisCodeOptions:
+            logger.warning('Undefined display; select display in gearbox settings before proceeding')
+            return
+
         currentAxisCodeIndexes = self.nmrResidueTableSettings.axisCodeOptions.getSelectedIndexes()
 
         with undoBlock():
