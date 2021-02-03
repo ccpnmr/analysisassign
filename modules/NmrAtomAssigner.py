8840291643a07b204142ac774a562b63509ee203
Ed Brooksbank <ejb66@le.ac.uk>
2021-02-03 18:11:36 +0000

    changed _displayOrderSpectrumDimensionIndices to something clearer - dimensionOrdering, but could be replaced by getByAxisCodes

--- modules/NmrAtomAssigner.py
+++ modules/NmrAtomAssigner.py
@@ -22,7 +22,7 @@ Reworked by EJB
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -32,8 +32,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-11-02 17:47:49 +0000 (Mon, November 02, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2021-02-03 18:11:36 +0000 (Wed, February 03, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -1269,7 +1269,7 @@ class NmrAtomAssignerModule(CcpnModule):
                          peakListView.peakList == peak.peakList]
 
         if peakListViews:
-            spectrumIndices = peakListViews[0].spectrumView._displayOrderSpectrumDimensionIndices
+            spectrumIndices = peakListViews[0].spectrumView.dimensionOrdering
 
             # for the 1D case, this is (0, None)
             if spectrumIndices[1] is None:
