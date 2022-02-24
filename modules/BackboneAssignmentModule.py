55fd7b73aeefb5edd456f8489b9c504fad0acafd
Ed Brooksbank <ejb66@le.ac.uk>
2022-02-24 11:54:21 +0000

    minor change to info string

--- modules/BackboneAssignmentModule.py
+++ modules/BackboneAssignmentModule.py
@@ -4,19 +4,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2022-01-06 16:27:56 +0000 (Thu, January 06, 2022) $"
-__version__ = "$Revision: 3.0.4 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2022-02-24 11:54:22 +0000 (Thu, February 24, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -511,7 +511,7 @@ class BackboneAssignmentModule(NmrResidueTableModule):
         #    assignMatrix = getNmrResidueMatches(queryShifts, self.allShifts, 'averageQScore')
 
         if not assignMatrix.values():
-            getLogger().info('No matches found for NmrResidue: %s' % nmrResidue.pid)
+            getLogger().info('No matches found for NmrResidue: %s - no matching isotopeCodes?' % nmrResidue.pid)
             return
         self._createMatchStrips(assignMatrix)
 
