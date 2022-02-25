557a0664e021b34e0336bed13164a0cef6e74c8b
Luca Mureddu <luca.mureddu@me.com>
2022-02-25 18:55:29 +0000

    Bug fixes on the IsotopeCode inconsistency.

--- modules/NmrAtomAssigner.py
+++ modules/NmrAtomAssigner.py
@@ -22,19 +22,19 @@ Reworked by EJB
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2021-12-14 22:03:54 +0000 (Tue, December 14, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-02-25 18:55:30 +0000 (Fri, February 25, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -53,7 +53,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from contextlib import contextmanager
 from ccpn.core.Peak import Peak
 from ccpn.core.NmrResidue import NmrResidue
-from ccpn.core.NmrAtom import NmrAtom
+from ccpn.core.NmrAtom import NmrAtom, UnknownIsotopeCode
 from ccpn.core.lib import Pid
 from ccpn.core.lib.AssignmentLib import isInterOnlyExpt, getNmrAtomPrediction, CCP_CODES
 from ccpn.core.lib.AssignmentLib import peaksAreOnLine
@@ -1176,6 +1176,9 @@ class NmrAtomAssignerModule(CcpnModule):
             for ii, axisCode in enumerate(peak.axisCodes):
                 if self.peakIndex[peak][ii] == index:
                     peak.assignDimension(axisCode, nmrAtom)
+                    isotopeCode = peak.peakList.spectrum.getByAxisCodes('isotopeCodes', [axisCode], exactMatch=True)[-1]
+                    if nmrAtom.isotopeCode in [UnknownIsotopeCode, None]:
+                        nmrAtom._setIsotopeCode(isotopeCode)
 
     def deassignAtomFromSelectedPeaks(self, peaks, nmrAtom):
         """Deassign the nmrAtom from the dimension
