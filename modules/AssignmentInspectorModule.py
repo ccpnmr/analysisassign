332835c9778dcabceaab65789b131cff7bf48dca
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-01 11:42:29 +0000

    wrong label for <Use all>

--- modules/AssignmentInspectorModule.py
+++ modules/AssignmentInspectorModule.py
@@ -18,7 +18,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-02-18 12:36:33 +0000 (Fri, February 18, 2022) $"
+__dateModified__ = "$dateModified: 2022-03-01 11:42:30 +0000 (Tue, March 01, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -59,7 +59,7 @@ from ccpn.ui.gui.widgets.SettingsWidgets import SpectrumDisplaySelectionWidget
 
 
 logger = getLogger()
-ALL = '<all>'
+ALL = '<Use all>'
 NMRRESIDUES = 'nmrResidues'
 NMRATOMS = 'nmrAtoms'
 
