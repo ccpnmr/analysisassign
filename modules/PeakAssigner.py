09376fb04247acd7669692fc3955036171d11916
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-05 11:51:22 +0100

    remove export/delete options from setcontextmenu - now defined in class init added methods to _SimplePandasTable.py moved methods to super-class

--- modules/PeakAssigner.py
+++ modules/PeakAssigner.py
@@ -12,12 +12,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-06-22 14:28:59 +0100 (Wed, June 22, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-05 11:51:22 +0100 (Mon, September 05, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -642,10 +642,11 @@ class AssignmentTable(GuiTable):
             data = {}
             self._clearSelectionCallbackFunction(data)
 
-    def _setContextMenu(self, enableExport=True, enableDelete=True):
+    def _setContextMenu(self):
         """Subclass guiTable to add new item to top of context menu
         """
-        super()._setContextMenu(enableExport=enableExport, enableDelete=enableDelete)
+        super()._setContextMenu()
+
         _actions = self.tableMenu.actions()
         if _actions:
             _topMenuItem = _actions[0]
