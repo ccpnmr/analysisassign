cd7bff2f85e89fc134aa15a43009c49500be55d3
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-14 16:38:02 +0100

    re-ordering to clean-up code for menus

--- modules/AssignmentInspectorModule.py
+++ modules/AssignmentInspectorModule.py
@@ -13,12 +13,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-07-27 12:46:05 +0100 (Wed, July 27, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-14 16:38:02 +0100 (Wed, September 14, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -360,13 +360,17 @@ class AssignmentInspectorModule(CcpnModule):
             # reset notifiers
             self.setBlankingAllNotifiers(False)
 
-    def navigateToNmrResidueCallBack(self, data):
+    def navigateToNmrResidueCallBack(self, selection, lastItem):
         """Navigate in selected displays to nmrResidue; skip if none defined
         """
         # handle a single chemicalShift - SHOULD always contain an object
-        objs = data[CallBack.OBJECT]
-        if not objs:
+        try:
+            if not (objs := list(lastItem[self._OBJECT])):
+                return
+        except Exception as es:
+            getLogger().debug2(f'{self.__class__.__name__}.navigateToNmrResidueCallBack: No selection\n{es}')
             return
+
         if isinstance(objs, (tuple, list)):
             chemicalShift = objs[0]
         else:
@@ -638,7 +642,7 @@ class _AssignmentInspectorTable(_NewChemicalShiftTable):
     """ChemicalShift table in AssignmentInspector module with modified behaviour
     """
 
-    def actionCallback(self, data):
+    def actionCallback(self, selection, lastItem):
         """Notifier DoubleClick action on item in table. Mark a chemicalShift based on all attached nmrAtoms
         """
         from ccpn.AnalysisAssign.modules.BackboneAssignmentModule import markNmrAtoms
@@ -651,10 +655,15 @@ class _AssignmentInspectorTable(_NewChemicalShiftTable):
             nmrAtoms = list(set(cs.nmrAtom for cs in cShifts if cs.nmrAtom))
             markNmrAtoms(self.mainWindow, nmrAtoms)
 
-    def selectionCallback(self, data):
+    def selectionCallback(self, selected, deselected, selection, lastItem):
         """Notifier Callback for selecting rows in the table
         """
-        objs = data[CallBack.OBJECT]
+        try:
+            objs = list(selection[self._OBJECT])
+
+        except Exception as es:
+            getLogger().debug2(f'{self.__class__.__name__}.selectionCallback: No selection\n{es}')
+
         self.current.chemicalShifts = objs or []
 
         if objs:
