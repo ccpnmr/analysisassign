b10220dbe6d57e2ad7f0ff56ad5e486dba529ad7
Ed Brooksbank <ejb66@le.ac.uk>
2023-08-16 11:31:37 +0100

    was still checking for <all>

--- modules/BackboneAssignmentModule.py
+++ modules/BackboneAssignmentModule.py
@@ -15,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-06-09 12:06:23 +0100 (Fri, June 09, 2023) $"
-__version__ = "$Revision: 3.1.1 $"
+__dateModified__ = "$dateModified: 2023-08-16 11:31:38 +0100 (Wed, August 16, 2023) $"
+__version__ = "$Revision: 3.2.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -56,7 +56,7 @@ from ccpn.util.decorators import logCommand
 from ccpn.util.Logging import getLogger
 
 
-ALL = '<all>'
+ALL = '<Use all>'
 MINMATCHES = 1
 MAXMATCHES = 20
 DEFAULTMATCHES = 3
