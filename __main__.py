79937eafac644112edbcb507ef9625e779256280
Daniel Thompson <dt264@le.ac.uk>
2023-11-14 18:29:23 +0000

    removed edit test

--- __main__.py
+++ __main__.py
@@ -2,3 +2,54 @@
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
+__reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
+                 "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2023-01-26 21:30:30 +0000 (Thu, January 26, 2023) $"
+__version__ = "$Revision: 3.1.1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: TJ $"
+__date__ = "$Date: 2017-04-07 10:28:40 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+import os
+import sys
+from PyQt5 import QtWidgets
+
+import ccpn
+from ccpn.framework.Application import defineProgramArguments
+from ccpn.AnalysisAssign.AnalysisAssign import Assign as Application
+
+
+def main():
+
+    # argument parser
+    parser = defineProgramArguments()
+
+    # add any additional commandline argument here
+    commandLineArguments = parser.parse_args()
+
+    os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--enable-logging --log-level=3"
+
+    application = Application(commandLineArguments)
+
+    application._startApplication()
+    QtWidgets.QApplication.quit()
+
+    if sys.platform.startswith('win'):
+        os._exit(0)
+
+
+if __name__ == '__main__':
+    main()
