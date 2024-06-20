638699010d0cf115f6eb10e38502671490207c82
Ed Brooksbank <ejb66@le.ac.uk>
2024-06-20 17:30:24 +0100

    hide logging

--- modules/PeakAssigner.py
+++ modules/PeakAssigner.py
@@ -18,8 +18,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-06-20 16:42:20 +0100 (Thu, June 20, 2024) $"
-__version__ = "$Revision: 3.2.3 $"
+__dateModified__ = "$dateModified: 2024-06-20 17:30:25 +0100 (Thu, June 20, 2024) $"
+__version__ = "$Revision: 3.2.4 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -132,7 +132,7 @@ class PeakAssigner(CcpnModule):
 
     # set the queue handling parameters
     _maximumQueueLength = 10
-    _logQueue = True
+    _logQueue = False
 
     def __init__(self, mainWindow, name="Peak Assigner"):
         """
