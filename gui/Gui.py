e5588d6407e1c81c76078e13740ea969e9eb7036
Geerten Vuister <gv29@leicester.ac.uk>
2024-09-12 08:50:01 +0100

    Moved showXYZ methods routines from AnalysisAssign to AnalysisAssign/gui/Gui

--- gui/Gui.py
+++ gui/Gui.py
@@ -16,7 +16,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2024-09-11 15:57:17 +0100 (Wed, September 11, 2024) $"
+__dateModified__ = "$dateModified: 2024-09-12 08:50:01 +0100 (Thu, September 12, 2024) $"
 __version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
@@ -33,6 +33,12 @@ from ccpn.ui.gui.menus.MenuDefs import \
     _projectHasSpectra, _projectHasPeaks
 from ccpn.ui.gui.menus._MenuItems import Menu, Action, Separator
 
+from ccpn.ui.gui.modules.CcpnModule import CcpnModule
+from ccpn.ui.gui.widgets import MessageDialog
+
+from ccpn.util.Logging import getLogger
+from ccpn.util.decorators import logCommand
+
 
 class AnalysisAssignGui(Gui):
     """Extended Gui interface for AnalysisAssign
@@ -45,28 +51,123 @@ class AnalysisAssignGui(Gui):
 
         app = self.application
         _assignMenu = \
-Menu("Assign",
-        Action("Set up NmrResidues", app.showSetupNmrResiduesPopup, shortcut = 'sn', checkEnabled=_projectHasSpectra),
-        Action("Pick and Assign", app.showPickAndAssignModule, shortcut = 'pa', checkEnabled=_projectHasSpectra),
+    Menu("Assign",
+
+        Action("Set up NmrResidues", self.showSetupNmrResiduesPopup, shortcut = 'sn', checkEnabled=_projectHasSpectra),
+        Action("Pick and Assign", self.showPickAndAssignModule, shortcut = 'pa', checkEnabled=_projectHasSpectra),
 
         Separator(),
-        Action("Backbone Assignment", app.showBackboneAssignmentModule, shortcut = 'bb', checkEnabled=_projectHasSpectra),
+        Action("Backbone Assignment", self.showBackboneAssignmentModule, shortcut = 'bb', checkEnabled=_projectHasSpectra),
         # Action("Sidechain Assignment", self.showSidechainAssignmentModule, shortcut = 'sc', enabled = False),
 
         Separator(),
-        Action("Peak Assigner", app.showPeakAssigner, shortcut = 'ap', checkEnabled=_projectHasPeaks),
-        Action("NmrAtom Assigner", app.showAtomSelector, shortcut = 'an', checkEnabled=_projectHasPeaks),
-        Action("Assignment Inspector", app.showAssignmentInspectorModule, shortcut = 'ai', checkEnabled=_projectHasPeaks),
-        # Action("Residue Information", app.showResidueInformation, shortcut = 'ri'),
-)  # end menu Assign
+        Action("Peak Assigner", self.showPeakAssigner, shortcut = 'ap', checkEnabled=_projectHasPeaks),
+        Action("NmrAtom Assigner", self.showAtomSelector, shortcut = 'an', checkEnabled=_projectHasPeaks),
+        Action("Assignment Inspector", self.showAssignmentInspectorModule, shortcut = 'ai', checkEnabled=_projectHasPeaks),
+        # Action("Residue Information", self.showResidueInformation, shortcut = 'ri'),
+
+    )  # end menu Assign
 
         # put it before the MACRO_MENU
         menuDefs.insertBefore([MACRO_MENU], menuDef=_assignMenu)
 
         # Add sequence graph to VIEW menu, before CHEMICAL_SHIFT_MAPPING
-        _seqGraphMenu = Action("Sequence Graph", app.showSequenceGraph, shortcut = 'sg')
+        _seqGraphMenu = Action("Sequence Graph", self.showSequenceGraph, shortcut = 'sg')
         menuDefs.insertBefore([VIEW_MENU, VIEW_CHEMICAL_SHIFT_MAPPING], menuDef=_seqGraphMenu)
 
         return menuDefs
 
+    def showSetupNmrResiduesPopup(self):
+        if not self.project.peakLists:
+            getLogger().warning('No peaklists in project. Cannot assign peaklists.')
+            MessageDialog.showWarning('No peaklists in project.', 'Cannot assign peaklists.')
+        else:
+            from ccpn.ui.gui.popups.SetupNmrResiduesPopup import SetupNmrResiduesPopup
+            popup = SetupNmrResiduesPopup(parent=self.mainWindow, mainWindow=self.mainWindow)
+            popup.exec_()
 
+    @logCommand('ui.')
+    def showPickAndAssignModule(self, position: str = 'bottom', relativeTo: CcpnModule = None):
+        """Display the Pick and Assign module.
+        """
+        from ccpn.AnalysisAssign.modules.PickAndAssignModule import PickAndAssignModule
+
+        mainWindow = self.mainWindow
+        pickAndAssignModule = PickAndAssignModule(mainWindow=mainWindow)
+        mainWindow._addModule(pickAndAssignModule, position=position, relativeTo=relativeTo)
+        return pickAndAssignModule
+
+    @logCommand('ui.')
+    def showBackboneAssignmentModule(self, position: str = 'bottom', relativeTo: CcpnModule = None):
+        """Display the Backbone Assignment module.
+        """
+        from ccpn.AnalysisAssign.modules.BackboneAssignmentModule import BackboneAssignmentModule
+
+        mainWindow = self.mainWindow
+        backboneModule = BackboneAssignmentModule(mainWindow=mainWindow)
+        mainWindow._addModule(backboneModule, position=position, relativeTo=relativeTo)
+        return backboneModule
+
+    @logCommand('ui.')
+    def showSidechainAssignmentModule(self, position: str = 'bottom', relativeTo: CcpnModule = None):
+        """Display the SideChain module.
+        """
+        MessageDialog.showWarning('Not implemented',
+                                  'Sidechain Assignment Module\n'
+                                  'is not implemented yet')
+
+    @logCommand('ui.')
+    def showPeakAssigner(self, position='bottom', relativeTo=None):
+        """Display the Peak Assigner module.
+        """
+        from ccpn.AnalysisAssign.modules.PeakAssigner import PeakAssigner
+
+        mainWindow = self.mainWindow
+        assignmentModule = PeakAssigner(mainWindow=mainWindow)
+        mainWindow._addModule(assignmentModule, position=position, relativeTo=relativeTo)
+        return assignmentModule
+
+    @logCommand('ui.')
+    def showAssignmentInspectorModule(self, nmrAtom=None, position: str = 'bottom', relativeTo: CcpnModule = None):
+        """Display the Assignment Inspector module.
+        """
+        from ccpn.AnalysisAssign.modules.AssignmentInspectorModule import AssignmentInspectorModule
+
+        mainWindow = self.mainWindow
+        assignmentInspectorModule = AssignmentInspectorModule(mainWindow=mainWindow, selectFirstItem=True)
+        mainWindow._addModule(assignmentInspectorModule, position=position, relativeTo=relativeTo)
+        return assignmentInspectorModule
+
+    @logCommand('ui.')
+    def showSequenceGraph(self, position: str = 'bottom', relativeTo: CcpnModule = None, nmrChain=None):
+        """Displays Sequence Graph at the bottom of the screen, relative to another module if nextTo is specified.
+        """
+        from ccpn.AnalysisAssign.modules.SequenceGraph import SequenceGraphModule
+
+        mainWindow = self.mainWindow
+        sequenceGraphModule = SequenceGraphModule(mainWindow=mainWindow, nmrChain=nmrChain)
+        mainWindow._addModule(sequenceGraphModule, position=position, relativeTo=relativeTo)
+        return sequenceGraphModule
+
+    @logCommand('ui.')
+    def showAtomSelector(self, position: str = 'bottom', relativeTo: CcpnModule = None, nmrAtom=None):
+        """Displays Atom Selector module.
+        """
+        from ccpn.AnalysisAssign.modules.NmrAtomAssigner import NmrAtomAssignerModule
+
+        mainWindow = self.mainWindow
+        nmrAtomAssigner = NmrAtomAssignerModule(mainWindow=mainWindow, nmrAtom=nmrAtom)
+        mainWindow._addModule(nmrAtomAssigner, position=position, relativeTo=relativeTo)
+        return nmrAtomAssigner
+
+    @logCommand('ui.')
+    def showPipeline(self, position='bottom', relativeTo=None):
+        """Display the Screening pipeLine Module
+        """
+        from ccpn.pipes import loadedPipes
+        from ccpn.ui.gui.modules.PipelineModule import GuiPipeline
+
+        mainWindow = self.mainWindow
+        guiPipeline = GuiPipeline(mainWindow=mainWindow, pipes=loadedPipes, templates=None)
+        mainWindow._addModule(guiPipeline, position=position, relativeTo=relativeTo)
+        return guiPipeline
